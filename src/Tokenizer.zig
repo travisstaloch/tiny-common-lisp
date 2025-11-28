c6cd7f3a22b5273f9dfd1514f3937044820fc8f7
Travis Staloch <1562827+travisstaloch@users.noreply.github.com>
2025-11-27 21:52:51 -0800

    rename from deme-lisp to tiny-common-lisp
    replace old src/root.zig and main.zig and update build files.  delete
    old files.
    
    * move Tokenizer to Tokenizer.zig

--- src/Tokenizer.zig
+++ src/Tokenizer.zig
@@ -1,91 +1,84 @@
-const std = @import("std");
-const assert = std.debug.assert;
+// TODO make streaming. replace src field with buffer and reader of some kind.
+// must be non seekable.
+const Tokenizer = @This();
 
 src: [:0]const u8,
+file_path: []const u8,
 pos: u32 = 0,
-mode: struct {
-    whitespace: Emit = .skip,
-    comments: Emit = .skip,
-    const Emit = enum { emit, skip };
-} = .{},
-
-const Tokenizer = @This();
 
 pub const Token = struct {
     start: u32,
     end: u32,
     tag: Tag,
 
-    pub const empty: Token = .{ .start = 0, .end = 0, .tag = .invalid };
-
-    pub const Tag = enum {
+    pub const Tag = enum(u8) {
         invalid,
         eof,
-        comment,
-        whitespace,
         lparen,
         rparen,
         symbol,
-        /// A double quoted string. The quotes are included
-        /// in the string.
-        string,
-        /// A number literal.
         number,
-        /// '
+        string,
         quote,
-        /// `
-        quasi_quote,
-        /// ,
-        quasi_unquote,
-        /// ,@
-        quasi_unquote_splicing,
+        dot,
     };
-
-    pub fn src(self: Token, s: [:0]const u8) []const u8 {
-        return s[self.start..self.end];
-    }
-
-    pub fn line(self: Token, s: [:0]const u8) []const u8 {
-        var start = self.start;
-        while (start != 0) {
-            start -= 1;
-            if (s[start] == '\n') break;
-        }
-        var end = self.end;
-        while (end < s.len) : (end += 1) {
-            if (s[end] == '\n') break;
-        }
-        return s[start + @intFromBool(start != 0) .. end];
-    }
 
     pub const Fmt = struct {
-        src: [:0]const u8,
-        token: Token,
+        t: Token,
+        tzr: *Tokenizer,
 
-        pub fn format(
-            self: @This(),
-            writer: *std.Io.Writer,
-        ) std.Io.Writer.Error!void {
-            const s = self.token.src(self.src);
-            if (self.token.tag == .string) {
-                try std.zig.stringEscape(s, writer);
-            } else try writer.writeAll(s);
+        pub fn format(f: Fmt, w: *Io.Writer) !void {
+            try w.print("{s}:{}:{} '{s}' {t}", .{
+                f.tzr.file_path,
+                f.t.start,
+                f.t.end,
+                f.tzr.src[f.t.start..f.t.end],
+                f.t.tag,
+            });
         }
     };
 
-    pub fn fmt(self: Token, s: [:0]const u8) Fmt {
-        return .{ .token = self, .src = s };
+    pub fn fmt(t: Token, tzr: *Tokenizer) Fmt {
+        return .{ .t = t, .tzr = tzr };
+    }
+
+    pub fn src(t: Token, s: [:0]const u8) []const u8 {
+        return s[t.start..t.end];
     }
 };
 
-fn isIdent(byte: u8) bool {
+const State = enum {
+    init,
+    symbol,
+    whitespace,
+    comment,
+    number,
+    number_dot,
+    string,
+    string_escape,
+};
+
+fn nextByte(t: *Tokenizer) u8 {
+    defer t.pos += @intFromBool(t.pos <= t.src.len);
+    return t.peek();
+}
+
+fn peek(t: *Tokenizer, n: u32) u8 {
+    return t.src[t.pos + n];
+}
+
+fn eof(t: *Tokenizer) bool {
+    return t.peek(0) == 0;
+}
+
+fn isSym(byte: u8) bool {
     return byte != ')' and byte != '(' and
         !std.ascii.isDigit(byte) and
         !isWhitespace(byte) and
         std.ascii.isPrint(byte);
 }
 
-fn isIdent2(byte: u8) bool {
+fn isSym2(byte: u8) bool {
     return byte != ')' and byte != '(' and
         !isWhitespace(byte) and
         std.ascii.isPrint(byte);
@@ -104,12 +97,8 @@ fn isWhitespace(byte: u8) bool {
     return @reduce(.Or, x == @as(V, @splat(byte)));
 }
 
-fn peek(self: *Tokenizer, offset: usize) u8 {
-    return self.src[self.pos + offset];
-}
-
 fn advance(self: *Tokenizer, n: u32) void {
-    self.pos += n;
+    self.pos += n * @intFromBool(self.src[self.pos] != 0);
 }
 
 fn advanceTo(self: *Tokenizer, tag: @Type(.enum_literal), n: u32, t: *Token) State {
@@ -120,75 +109,56 @@ fn advanceTo(self: *Tokenizer, tag: @Type(.enum_literal), n: u32, t: *Token) Sta
         .rparen,
         .invalid,
         .quote,
-        .quasi_quote,
-        .quasi_unquote,
-        .quasi_unquote_splicing,
+        .dot,
+        // .quasi_quote,
+        // .quasi_unquote,
+        // .quasi_unquote_splicing,
         => .init,
         else => |ttag| @field(State, @tagName(ttag)),
     };
 }
 
-const State = enum {
-    init,
-    comment,
-    symbol,
-    whitespace,
-    number,
-    string,
-    string_escape,
-};
-
 pub fn next(self: *Tokenizer) Token {
-    // std.debug.print("next> pos {}\n", .{self.pos});
-    // defer std.debug.print("next< pos {}\n", .{self.pos});
-    var t: Token = .{ .start = self.pos, .end = self.pos, .tag = .invalid };
+    var t: Token = undefined;
 
     state: switch (State.init) {
         inline else => |state| {
             const byte = self.peek(0);
-            // std.debug.print("  state {t} byte '{c}'\n", .{ state, byte });
+            // trace("  state {t} byte '{c}'", .{ state, byte });
             switch (state) {
-                .init => switch (byte) {
-                    0 => {
-                        t.tag = .eof;
-                        break :state;
-                    },
-                    ';' => continue :state self.advanceTo(.comment, 1, &t),
-                    '(' => _ = self.advanceTo(.lparen, 1, &t),
-                    ')' => _ = self.advanceTo(.rparen, 1, &t),
-                    '\'' => _ = self.advanceTo(.quote, 1, &t),
-                    '`' => _ = self.advanceTo(.quasi_quote, 1, &t),
-                    ',' => _ = if (self.peek(1) == '@')
-                        self.advanceTo(.quasi_unquote_splicing, 2, &t)
-                    else
-                        self.advanceTo(.quasi_unquote, 1, &t),
-                    '0'...'9' => continue :state self.advanceTo(.number, 1, &t),
-                    '-', '+' => {
-                        switch (self.peek(1)) {
-                            '1'...'9' => continue :state self.advanceTo(.number, 1, &t),
-                            '0' => self.pos += 2,
-                            else => continue :state self.advanceTo(.symbol, 1, &t),
-                        }
-                    },
-                    '"' => continue :state self.advanceTo(.string, 1, &t),
-                    else => if (isIdent(byte)) {
-                        continue :state self.advanceTo(.symbol, 1, &t);
-                    } else if (isWhitespace(byte)) {
-                        continue :state self.advanceTo(.whitespace, 1, &t);
-                    } else std.debug.panic("unexpected byte '{c}'\n", .{byte}),
+                .init => {
+                    t = .{ .tag = .invalid, .start = self.pos, .end = self.pos };
+                    switch (byte) {
+                        0 => t.tag = .eof,
+                        '(' => _ = self.advanceTo(.lparen, 1, &t),
+                        ')' => _ = self.advanceTo(.rparen, 1, &t),
+                        '\'' => _ = self.advanceTo(.quote, 1, &t),
+                        '.' => _ = self.advanceTo(.dot, 1, &t),
+                        ';' => continue :state self.advanceTo(.comment, 1, &t),
+                        '0'...'9' => continue :state self.advanceTo(.number, 1, &t),
+                        '"' => continue :state self.advanceTo(.string, 1, &t),
+                        '-', '+' => {
+                            switch (self.peek(1)) {
+                                '1'...'9' => continue :state self.advanceTo(.number, 1, &t),
+                                '0' => self.pos += 2,
+                                else => continue :state self.advanceTo(.symbol, 1, &t),
+                            }
+                        },
+                        else => if (isSym(byte)) {
+                            continue :state self.advanceTo(.symbol, 1, &t);
+                        } else if (isWhitespace(byte)) {
+                            continue :state self.advanceTo(.whitespace, 1, &t);
+                        } else {
+                            std.debug.panic("unexpected byte '{c}'", .{byte});
+                        },
+                    }
                 },
                 .whitespace => if (isWhitespace(byte)) {
                     continue :state self.advanceTo(.whitespace, 1, &t);
                 } else {
-                    if (self.mode.whitespace == .skip) {
-                        t = .{
-                            .tag = .invalid,
-                            .start = self.pos,
-                            .end = self.pos,
-                        };
-                        continue :state .init;
-                    }
+                    continue :state .init;
                 },
+
                 .comment => {
                     self.advance(1);
                     switch (byte) {
@@ -200,22 +170,21 @@ pub fn next(self: *Tokenizer) Token {
                         },
                         0 => {},
                         else => continue :state .comment,
-                        // else => std.debug.panic("unexpected byte '{c}'\n", .{byte}),
                     }
-                    if (self.mode.comments == .skip) {
-                        t = .{
-                            .tag = .invalid,
-                            .start = self.pos,
-                            .end = self.pos,
-                        };
-                        continue :state .init;
-                    }
+                    continue :state .init;
                 },
-                .symbol => if (isIdent2(byte)) {
+                .symbol => if (isSym2(byte)) {
                     continue :state self.advanceTo(.symbol, 1, &t);
                 },
                 .number => if (std.ascii.isDigit(byte)) {
                     continue :state self.advanceTo(.number, 1, &t);
+                } else if (byte == '.') {
+                    continue :state self.advanceTo(.number_dot, 1, &t);
+                } else {
+                    t.tag = .number;
+                },
+                .number_dot => if (std.ascii.isDigit(byte)) {
+                    continue :state self.advanceTo(.number_dot, 1, &t);
                 } else {
                     t.tag = .number;
                 },
@@ -236,41 +205,10 @@ pub fn next(self: *Tokenizer) Token {
             }
         },
     }
-
     t.end = self.pos;
+    // trace("{f}", .{t.fmt(self)});
     return t;
 }
 
-const testing = std.testing;
-
-test "tokenize an expression" {
-    const src =
-        \\ (begin (print "str" 5 `(,a ,@b)))
-    ;
-    var t: Tokenizer = .{ .src = src };
-    try testing.expectEqual(.lparen, t.next().tag);
-    {
-        const tok = t.next();
-        try testing.expectEqual(.symbol, tok.tag);
-        try testing.expectEqualStrings("begin", tok.src(src));
-    }
-    try testing.expectEqual(.lparen, t.next().tag);
-    try testing.expectEqual(.symbol, t.next().tag);
-    try testing.expectEqual(.string, t.next().tag);
-    {
-        const tok = t.next();
-        try testing.expectEqual(.number, tok.tag);
-        try testing.expectEqualStrings("5", tok.src(src));
-    }
-    try testing.expectEqual(.quasi_quote, t.next().tag);
-    try testing.expectEqual(.lparen, t.next().tag);
-    try testing.expectEqual(.quasi_unquote, t.next().tag);
-    try testing.expectEqual(.symbol, t.next().tag);
-    try testing.expectEqual(.quasi_unquote_splicing, t.next().tag);
-    try testing.expectEqual(.symbol, t.next().tag);
-    try testing.expectEqual(.rparen, t.next().tag);
-    try testing.expectEqual(.rparen, t.next().tag);
-    try testing.expectEqual(.rparen, t.next().tag);
-    try testing.expectEqual(.eof, t.next().tag);
-    try testing.expectEqual(.eof, t.next().tag);
-}
+const std = @import("std");
+const Io = std.Io;
--- src/main.zig
+++ src/main.zig
@@ -1,44 +1,135 @@
 const std = @import("std");
-const assert = std.debug.assert;
-const deme = @import("deme");
 const flagset = @import("flagset");
-const anyline = @import("anyline");
+
+const TinyLisp = @import("root.zig");
+
+pub const std_options: std.Options = .{
+    // .log_level = .debug,
+};
 
 const flags = [_]flagset.Flag{
-    .{ .type = []const u8, .name = "file", .options = .{ .kind = .positional } },
+    .init([]const u8, "script", .{
+        .short = 's',
+        .desc = "file path of script to run",
+        .kind = .list,
+    }),
+    .init([]const u8, "eval", .{ .short = 'e', .desc = "text to eval", .kind = .list }),
+    .init([]const u8, "load", .{ .short = 'l', .desc = "file to load", .kind = .list }),
+    .init(bool, "banner", .{ .desc = "show banner", .default_value_ptr = &true }),
 };
 
 pub fn main() !void {
-    var gpa_state = std.heap.GeneralPurposeAllocator(.{}){};
-    defer assert(gpa_state.deinit() == .ok);
-    const gpa = gpa_state.allocator();
+    var alloc_buf: [4096 * 8]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&alloc_buf);
+    const gpa = fba.allocator();
+
+    var stdout_buf: [256]u8 = undefined;
+    var stdout_w = std.fs.File.stdout().writer(&stdout_buf);
+    const stdout = &stdout_w.interface;
+    defer stdout.flush() catch {};
 
     var args = flagset.parseFromIter(&flags, std.process.args(), .{ .allocator = gpa }) catch |e| switch (e) {
-        error.HelpRequested => return,
+        error.HelpRequested => {
+            try stdout.print("{f}", .{flagset.fmtUsage(&flags, ": <35", .full,
+                \\
+                \\usage: tinylisp <options>
+                \\
+                \\
+            )});
+            try stdout.flush();
+            return;
+        },
         else => return e,
     };
     defer args.deinit(gpa);
-    // std.debug.print("args {f}\n", .{flagset.fmtParsed(&flags, args.parsed, .{})});
-    const f = try std.fs.cwd().openFile(args.parsed.file, .{});
-    defer f.close();
-    var read_buf: [256]u8 = undefined;
-    var freader = f.reader(&read_buf);
-    var w: std.Io.Writer.Allocating = try .initCapacity(gpa, try freader.getSize());
-    defer w.deinit();
-    try freader.interface.streamExact(&w.writer, w.writer.buffer.len);
-    const end = w.writer.end;
-    try w.writer.writeByte(0);
-    const src = w.written()[0..end :0];
-    var p = try deme.Parser.init(src, .{ .mode = .{ .gpa = gpa } });
-    var res = try p.parse();
-    defer res.ast.deinit();
-    std.debug.print("{f}\n{f}\n{f}\n", .{ res.ast.root_env.iterator(&res.ast), res.ast.root_lst.iterator(&res.ast), res.ast.dump(src) });
-    anyline.using_history();
 
-    var arena = std.heap.ArenaAllocator.init(gpa);
-    while (true) {
-        const line = try anyline.readline(arena.allocator(), " > ");
-        std.debug.print("{s}", .{line});
-        try anyline.add_history(gpa, line);
+    if (args.parsed.banner) {
+        try stdout.print(
+            // banner genrated at
+            // https://patorjk.com/software/taag/#p=display&f=Small+Block&t=TinyLisp&x=none&v=4&h=4&w=80&we=false
+            \\
+            \\  ▀▛▘▗       ▌  ▗       
+            \\   ▌ ▄ ▛▀▖▌ ▌▌  ▄ ▞▀▘▛▀▖
+            \\   ▌ ▐ ▌ ▌▚▄▌▌  ▐ ▝▀▖▙▄▘
+            \\   ▘ ▀▘▘ ▘▗▄▘▀▀▘▀▘▀▀ ▌  
+            \\    TinyLisp 0.0.1
+            \\
+            \\args: {f}
+            \\
+            \\
+        , .{flagset.fmtParsed(&flags, args.parsed, .{})});
+        try stdout.flush();
+    }
+
+    var memory: [1024 * 16]TinyLisp.Expr = undefined;
+    var p: TinyLisp = .init(&memory, stdout, .quiet);
+
+    if (args.parsed.eval.items.len > 0) {
+        for (args.parsed.eval.items) |eval| {
+            try run(gpa, &p, "<eval>", .{ .eval = eval });
+        }
+    }
+    if (args.parsed.load.items.len > 0) {
+        unreachable;
+    }
+    if (args.parsed.script.items.len > 0) {
+        for (args.parsed.script.items) |script| {
+            const input_f = std.fs.cwd().openFile(script, .{}) catch |e| {
+                std.debug.print("failed to open file: {s}: error.{t}\n", .{ script, e });
+                break;
+            };
+            defer input_f.close();
+            try run(gpa, &p, script, .{ .script = input_f });
+        }
+    } else {
+        try run(gpa, &p, "<stdin>", .{ .repl = std.fs.File.stdin() });
+    }
+}
+
+fn run(
+    gpa: std.mem.Allocator,
+    p: *TinyLisp,
+    name: []const u8,
+    mode: union(enum) {
+        script: std.fs.File,
+        repl: std.fs.File,
+        eval: []const u8,
+    },
+) !void {
+    var wa: std.Io.Writer.Allocating = .init(gpa);
+    var input_buf: [4096]u8 = undefined;
+
+    switch (mode) {
+        .repl => |input_f| while (true) {
+            p.print_mode = .repl;
+            try p.w.print("{: >4}> ", .{p.sp - p.hp / 8});
+            try p.w.flush();
+            wa.clearRetainingCapacity();
+
+            while (true) {
+                const amt = try input_f.read(&input_buf);
+                if (amt == 0 or (amt == 1 and input_buf[0] == '\n')) break;
+                try wa.writer.writeAll(input_buf[0..amt]);
+            }
+            try wa.writer.writeByte(0);
+            const src = wa.written()[0 .. wa.written().len - 1 :0];
+            if (src.len <= 1) break;
+
+            _ = try p.run(src, name);
+            p.gcOld();
+        },
+        .script => |input_f| {
+            var input = input_f.reader(&input_buf);
+            _ = try input.interface.stream(&wa.writer, .unlimited);
+            try wa.writer.writeByte(0);
+            const src = wa.written()[0 .. wa.written().len - 1 :0];
+            _ = try p.run(src, name);
+        },
+        .eval => |to_eval| {
+            try wa.writer.writeAll(to_eval);
+            try wa.writer.writeByte(0);
+            const src = wa.written()[0 .. wa.written().len - 1 :0];
+            _ = try p.run(src, name);
+        },
     }
 }
