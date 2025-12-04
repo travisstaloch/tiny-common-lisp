fcce88d65cf68ac3dd0f2468bdf9996dd70e75ad
Travis Staloch <1562827+travisstaloch@users.noreply.github.com>
2025-12-04 13:42:47 -0800

    introduce region allocation
    only using rootRegion() as a bump allocator with checkpoint/restore in main so far
    
    * replace stack/heap memory model with region based allocation
    * store atoms in a string hash map instead of in the heap
    * use Expr.Id everywhere instead of Expr
    * rework how apply() builds args - use new Iterator and consFromSlice()

--- src/tests.zig
+++ src/tests.zig
@@ -6,10 +6,12 @@ const Expr = Interp.Expr;
 
 fn expectExprEqual(expected: Expr.Fmt, actual: Expr.Fmt) !void {
     // trace("{f}\n{f}\n", .{ expected, actual });
-    try testing.expectEqual(expected.e.boxed.tag, actual.e.boxed.tag);
+    const e = expected.e.expr(expected.p);
+    const a = actual.e.expr(actual.p);
+    try testing.expectEqual(e.boxed.tag, a.boxed.tag);
     const ep = expected.p;
     const ap = actual.p;
-    switch (expected.e.boxed.tag.int()) {
+    switch (e.boxed.tag.int()) {
         Interp.ATOM => try testing.expectEqualStrings(
             ep.atomName(expected.e),
             ap.atomName(actual.e),
@@ -24,7 +26,7 @@ fn expectExprEqual(expected: Expr.Fmt, actual: Expr.Fmt) !void {
                 ap.cdrAssume(actual.e).fmt(ap),
             );
         },
-        else => try testing.expectEqual(expected.e.int, actual.e.int),
+        else => try testing.expectEqual(e.int, a.int),
     }
 }
 
@@ -35,9 +37,9 @@ const TestCase = union(enum) {
 
 fn testParseFmt(c: TestCase) !void {
     var discarding = std.Io.Writer.Discarding.init(&.{});
-    const N = 4096 * 4;
-    var memory: [N]Expr = undefined;
-    var l: Interp = .init(&memory, &discarding.writer, .quiet);
+    const N = 4096 * 4 * @sizeOf(Expr);
+    var buffer: [N]u8 align(16) = undefined;
+    var p: Interp = try .init(&buffer, &discarding.writer, .quiet);
     const src = switch (c) {
         .file_path => try std.fs.cwd().readFileAllocOptions(t_gpa, c.file_path, 100000, null, .of(u8), 0),
         .src => try t_gpa.dupeZ(u8, c.src),
@@ -47,16 +49,16 @@ fn testParseFmt(c: TestCase) !void {
         .src => "test",
     };
     defer t_gpa.free(src);
-    const e = try l.parse(src, file_path);
+    const e = try p.parse(src, file_path);
     // trace("{s} {s}", .{ file_path, src });
     // trace("e {f}", .{e.fmt(&l)});
-    const src2 = try std.fmt.allocPrintSentinel(t_gpa, "{f}", .{e.fmt(&l)}, 0);
+    const src2 = try std.fmt.allocPrintSentinel(t_gpa, "{f}", .{e.fmt(&p)}, 0);
     // trace("{s}", .{src2});
     defer t_gpa.free(src2);
-    var memory2: [N]Expr = undefined;
-    var l2: Interp = .init(&memory2, &discarding.writer, .quiet);
-    const e2 = try l2.parse(src2, file_path);
-    try expectExprEqual(e.fmt(&l), e2.fmt(&l2));
+    var memory2: [N]u8 align(16) = undefined;
+    var p2: Interp = try .init(&memory2, &discarding.writer, .quiet);
+    const e2 = try p2.parse(src2, file_path);
+    try expectExprEqual(e.fmt(&p), e2.fmt(&p2));
 }
 
 test "parse fmt rountrip" {
@@ -69,11 +71,25 @@ test "parse fmt rountrip" {
 
 pub fn testEval(expected: []const u8, src: [:0]const u8) !void {
     var discarding = std.Io.Writer.Discarding.init(&.{});
-    const N = 4096 * 4;
-    var memory: [N]Expr = undefined;
-    var l: Interp = .init(&memory, &discarding.writer, .quiet);
-    const e = try l.run(src, "<testEval>");
-    try testing.expectFmt(expected, "{f}", .{e.fmt(&l)});
+    const N = 4096 * 4 * @sizeOf(Expr);
+    var memory: [N]u8 align(16) = undefined;
+    var p: Interp = try .init(&memory, &discarding.writer, .quiet);
+    const e = try p.run(src, "<testEval>");
+    try testing.expectFmt(expected, "{f}", .{e.fmt(&p)});
+}
+
+test "car cdr" {
+    var discarding = std.Io.Writer.Discarding.init(&.{});
+    const N = 4096 * 4 * @sizeOf(Expr);
+    var memory: [N]u8 align(16) = undefined;
+    var p: Interp = try .init(&memory, &discarding.writer, .quiet);
+    const a = try p.atom("a");
+    const b = try p.atom("b");
+    const c = try p.cons(a, b);
+    try testing.expectEqual(a.expr(&p).*, (try p.car(c)).expr(&p).*);
+    try testing.expectEqual(b.expr(&p).*, (try p.cdr(c)).expr(&p).*);
+    try testing.expectEqual(null, p.cdrOpt(b));
+    try testing.expectEqual(null, p.cdrOpt(try p.cdr(c)));
 }
 
 test "nil sym" {
